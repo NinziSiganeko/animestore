0b3f9a785c647e0025b58a67c4a8a9627d800463
220094896 <220094896@mycput.ac.za>
2025-05-10 20:48:52 +0200

    order.java

--- src/main/java/za/ac/cput/domain/Order.java
+++ src/main/java/za/ac/cput/domain/Order.java
@@ -1,40 +1,51 @@
 package za.ac.cput.domain;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Order {
     private int orderId ;
-    private Date orderDate;
+    private LocalDate orderDate;
     private String status;
 
-    public Order(Order builder) {
+    private Order(){
+
+    }
+
+    public Order(Builder builder) {
         this.orderId = builder.orderId;
         this.orderDate = builder.orderDate;
         this.status = builder.status;
 
     }
-    public int getOrderId() {
-        return orderId;
+    public int getOrderId() {return orderId;}
+
+    public LocalDate getOrderDate() {return orderDate;}
+    public String getStatus() {return status;}
+
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", orderDate=" + orderDate +
+                ", status='" + status + '\'' +
+                '}';
     }
-    public Date getOrderDate() {
-        return orderDate;
-    }
-    public String getStatus() {
-        return status;
-    }
+
 
     public static class Builder {
         private int orderId;
-        private Date orderDate;
-        private String status;
 
+        private LocalDate orderDate;
+
+        private String status;
 
         public Builder setOrderId(int orderId) {
             this.orderId = orderId;
             return this;
         }
 
-        public Builder setOrderDate(Date orderDate) {
+        public Builder setOrderDate(LocalDate orderDate) {
             this.orderDate = orderDate;
             return this;
         }
@@ -47,24 +58,23 @@ public class Order {
 
     }
         public Order copy(Order order) {
-            this.orderId = order.getOrderId();
-            this.orderDate = order.getOrderDate();
+            this.orderId = order.getOrderId ();
+            this.orderDate = order.getOrderDate() ;
             this.status = order.getStatus();
             return this;
         }
 
 
 
-    public Order build() {
-        return new Order(this);
-    }
-
-
-
-
-
-
-
+      public Order build() {return new Order(this);}
 
 }
+
+
+
+
+
+
+
+
 
