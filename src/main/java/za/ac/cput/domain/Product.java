e39061685cfdc01b31f5bae2b7a487355e2c8945
221230793 <221230793@mycput.ac.za>
2025-05-18 21:29:02 +0200

    animestore

--- src/main/java/za/ac/cput/domain/Product.java
+++ src/main/java/za/ac/cput/domain/Product.java
@@ -1,86 +1,39 @@
-/* Product.java
-   Product POJO class based on UML diagram
-   Author: onthatile 221230793
-   Date: 11 May 2025
-*/
-
 package za.ac.cput.domain;
 
 public class Product {
-    private int productId;
+    private String productId;
     private String name;
-    private String description;
     private double price;
     private int stock;
 
-    private Product() {}
-
-    public Product(Builder builder) {
+    // Private constructor using Builder
+    private Product(Builder builder) {
         this.productId = builder.productId;
         this.name = builder.name;
-        this.description = builder.description;
         this.price = builder.price;
         this.stock = builder.stock;
     }
 
-    public int getProductId() {
-        return productId;
-    }
+    // Getters
+    public String getProductId() { return productId; }
+    public String getName() { return name; }
+    public double getPrice() { return price; }
+    public int getStock() { return stock; }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    // Optional: method to update stock (from UML)
-    public void updateStock() {
-        // Placeholder for logic, e.g. reduce by 1 or based on OrderItem
-        if (stock > 0) {
-            stock--;
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "productId=" + productId +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", stock=" + stock +
-                '}';
-    }
-
+    // Static Builder class
     public static class Builder {
-        private int productId;
+        private String productId;
         private String name;
-        private String description;
         private double price;
         private int stock;
 
-        public Builder setProductId(int productId) {
+        public Builder setProductId(String productId) {
             this.productId = productId;
             return this;
         }
 
         public Builder setName(String name) {
             this.name = name;
-            return this;
-        }
-
-        public Builder setDescription(String description) {
-            this.description = description;
             return this;
         }
 
@@ -91,15 +44,6 @@ public class Product {
 
         public Builder setStock(int stock) {
             this.stock = stock;
-            return this;
-        }
-
-        public Builder copy(Product product) {
-            this.productId = product.productId;
-            this.name = product.name;
-            this.description = product.description;
-            this.price = product.price;
-            this.stock = product.stock;
             return this;
         }
 
--- src/main/java/za/ac/cput/domain/ProductCategory.java
+++ src/main/java/za/ac/cput/domain/ProductCategory.java
@@ -1,23 +1,16 @@
-/* ProductCategory.java
-   ProductCategory POJO class based on UML diagram
-   Author: onthatile 221230793
-   Date: 11 May 2025
-*/
-
 package za.ac.cput.domain;
 
 public class ProductCategory {
-    private int categoryId;
+    private String categoryId;
     private String categoryName;
 
-    private ProductCategory() {}
-
-    public ProductCategory(Builder builder) {
+    // Private constructor
+    private ProductCategory(Builder builder) {
         this.categoryId = builder.categoryId;
         this.categoryName = builder.categoryName;
     }
 
-    public int getCategoryId() {
+    public String getCategoryId() {
         return categoryId;
     }
 
@@ -25,31 +18,18 @@ public class ProductCategory {
         return categoryName;
     }
 
-    @Override
-    public String toString() {
-        return "ProductCategory{" +
-                "categoryId=" + categoryId +
-                ", categoryName='" + categoryName + '\'' +
-                '}';
-    }
-
+    // Builder class
     public static class Builder {
-        private int categoryId;
+        private String categoryId;
         private String categoryName;
 
-        public Builder setCategoryId(int categoryId) {
+        public Builder setCategoryId(String categoryId) {
             this.categoryId = categoryId;
             return this;
         }
 
         public Builder setCategoryName(String categoryName) {
             this.categoryName = categoryName;
-            return this;
-        }
-
-        public Builder copy(ProductCategory category) {
-            this.categoryId = category.getCategoryId();
-            this.categoryName = category.getCategoryName();
             return this;
         }
 
