c3a16cfe4012e3a3827a0b5b171298fbe8307669
220094896 <220094896@mycput.ac.za>
2025-05-26 09:06:26 +0200

    Update

--- src/main/java/za/ac/cput/Repository/OrderRepository.java
+++ src/main/java/za/ac/cput/Repository/OrderRepository.java
@@ -1,4 +1,13 @@
 package za.ac.cput.Repository;
 
-public class OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.domain.Order;
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+     List<Order> findByStatus(String status);
 }
+
