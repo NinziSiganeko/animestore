aa8b8d2f40b658b9db46a1c7cedb69830052dcd7
221230793 <221230793@mycput.ac.za>
2025-05-26 19:51:28 +0200

    animestore

--- src/main/java/za/ac/cput/Service/impl/ProductCategoryServiceImpl.java
+++ src/main/java/za/ac/cput/Service/impl/ProductCategoryServiceImpl.java
@@ -3,7 +3,7 @@ package za.ac.cput.Service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.ac.cput.domain.ProductCategory;
-import za.ac.cput.repository.ProductCategoryRepository;
+import za.ac.cput.Repository.ProductCategoryRepository;
 import za.ac.cput.Service.ProductCategoryService;
 
 import java.util.List;
