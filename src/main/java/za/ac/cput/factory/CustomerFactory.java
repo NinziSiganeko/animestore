6bc4e48abe0cd4d4b80cfa2a4753937b3a8d824c
veronica <222914556@mycput.ac.za>
2025-08-06 07:25:51 +0200

    Updated POJO

--- src/main/java/za/ac/cput/factory/CustomerFactory.java
+++ src/main/java/za/ac/cput/factory/CustomerFactory.java
@@ -2,27 +2,26 @@ package za.ac.cput.factory;
 
 import za.ac.cput.domain.Customer;
 import za.ac.cput.util.Helper;
-import za.ac.cput.domain.Customer;
-import za.ac.cput.util.Helper;
 
 public class CustomerFactory {
-        public static Customer createCustomer(Long userId, String username, String password, String email, String address, String phoneNumber) {
-            if(Helper.isNullorEmpty(username) ||
-                    Helper.isNullorEmpty(password) ||
-                    Helper.isNullorEmpty(address) ||
-                    Helper.isNullorEmpty(phoneNumber))
 
-                return null;
-            if(!Helper.isNullorEmpty(email))
-                return null;
-
-            return new Customer.Builder()
-                    .setUserId(userId)
-                    .setUsername(username)
-                    .setPassword(password)
-                    .setEmail(email)
-                    .setAddress(address)
-                    .setPhoneNumber(phoneNumber)
-                    .build();
+    public static Customer createCustomer(Long userId, String username, String password, String email, String address, String phoneNumber) {
+        if (Helper.isNullorEmpty(username) ||
+                Helper.isNullorEmpty(password) ||
+                Helper.isNullorEmpty(email) ||
+                Helper.isNullorEmpty(address) ||
+                Helper.isNullorEmpty(phoneNumber)) {
+            return null;
         }
+
+        return new Customer.Builder()
+                .setUserId(userId)
+                .setUsername(username)
+                .setPassword(password)
+                .setEmail(email)
+                .setAddress(address)
+                .setPhoneNumber(phoneNumber)
+                .build();
     }
+}
+
