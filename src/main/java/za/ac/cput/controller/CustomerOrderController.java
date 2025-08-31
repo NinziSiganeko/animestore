47167c7ea97a7d11e67da371b50d70031c1087c0
220094896 <220094896@mycput.ac.za>
2025-08-31 22:09:06 +0200

    Delivery Update

---
+++ src/main/java/za/ac/cput/controller/CustomerOrderController.java
@@ -0,0 +1,42 @@
+package za.ac.cput.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import za.ac.cput.domain.Customer;
+import za.ac.cput.domain.CustomerOrder;
+import za.ac.cput.service.CustomerOrderService;
+import za.ac.cput.service.CustomerService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customerOrder")
+public class CustomerOrderController {
+    @Autowired
+    CustomerOrderService service;
+
+    @PostMapping("/create")
+    public CustomerOrder create(@RequestBody CustomerOrder customer) {
+        return service.create(customer);
+    }
+
+    @GetMapping("/read/{customerOrderId}")
+    public CustomerOrder read(@PathVariable int customerOrderId){
+        return service.read(customerOrderId);
+    }
+
+    @PostMapping("/update")
+    public CustomerOrder update(@RequestBody CustomerOrder customer) {
+        return service.update(customer);
+    }
+
+    @DeleteMapping("/delete/{customerOrderId}")
+    public void delete(@PathVariable int customerOrderId){
+        service.delete(customerOrderId);
+    }
+
+    @GetMapping("/getAll")
+    public List<CustomerOrder> getAll(){
+        return service.getAll();
+    }
+}
