d61f90e6b59d2c8b3bf7e25e6c0903424a02e2d3
Siwaphiwe220030898 <220030898@mycput.ac.za>
2025-08-31 23:05:32 +0200

    InventoryController, Factory and Service Tests

---
+++ src/test/java/za/ac/cput/factory/InventoryFactoryTest.java
@@ -0,0 +1,45 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.Inventory;
+import za.ac.cput.domain.Product;
+import za.ac.cput.domain.ProductStatus;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryFactoryTest {
+    Product product = ProductFactory.createProduct("P001", "Manga Book", 199.99, 50);
+
+    public Inventory inventory1 = InventoryFactory.createInventory(product, ProductStatus.Few_IN_STOCK);
+    public Inventory inventory2 = InventoryFactory.createInventory(product, ProductStatus.Few_IN_STOCK);
+    public Inventory inventory3 = InventoryFactory.createInventory(product, ProductStatus.DISCONTINUED); // Invalid test
+
+    @Test
+    @Order(1)
+    public void testCreateInventory() {
+        assertNotNull(inventory1);
+        System.out.println(inventory1.toString());
+    }
+
+    @Test
+    @Order(2)
+    public void testCreateInventoryWithAllAttributes() {
+        assertNotNull(inventory2);
+        System.out.println(inventory2.toString());
+    }
+
+    @Test
+    @Order(3)
+    public void testCreateInventoryThatFails() {
+        assertNotNull(inventory3);
+        System.out.println(inventory3);
+    }
+
+    @Test
+    @Disabled
+    @Order(4)
+    public void testNotImplemented() {
+    }
+}
