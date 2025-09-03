1b6852af36780745d9806934a064351955784bda
220094896 <220094896@mycput.ac.za>
2025-09-03 06:14:56 +0200

    update

--- src/main/java/za/ac/cput/repository/CustomerOrderRepository.java
+++ src/main/java/za/ac/cput/repository/CustomerOrderRepository.java
@@ -7,6 +7,5 @@ import java.util.List;
 
 @Repository
 public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Integer> {
-
     List<CustomerOrder> findByStatus(String status);
 }
